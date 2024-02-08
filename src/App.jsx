d59e8136c8cb8a605377e047c5f5673b8b99d776
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-08 09:56:04 -0500

    Added API fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
 import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from "react";
 import Navbar from './Navbar';
+import ErrorPage from './ErrorPage';
 import './App.css'
 
+// TODO: Testing
+
 function App(){
+
+    const [error, setError] = useState(null);
+    const [APIData, setAPIData] = useState('');
+
+    async function fetchAPIData(){
+        try{
+            const response = await fetch('https://fakestoreapi.com/products/category/electronics');
+            if (!response.ok) throw new Error("Failed to get API Data")
+            const data = await response.json();
+            setAPIData(data);
+        }catch(err){
+            setError(err.message)
+        }
+    }
+
+    useEffect(() => {
+        fetchAPIData();
+    }, [])
+
     return (
         <>
             <Navbar />
-            <Outlet />
+            {error? (
+                <div style={{fontSize: "40px"}}>Error: Could not load data from API! 🥹</div>
+                ) : <Outlet context={APIData}/>}
         </>
     )
 }
--- src/Shop.jsx
+++ src/Shop.jsx
@@ -1,4 +1,9 @@
+import { useOutletContext } from "react-router-dom";
+
+
 function Shop(){
+    const APIData = useOutletContext(); // Defined from App.jsx
+    
     return(
         <div>SHOP</div>
     )
