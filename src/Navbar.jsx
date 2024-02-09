68b4d88ed1827528a4779135cf2126e1009bad26
Sebastian Hothaza <sebastianhothaza@gmail.com>
2024-02-09 09:31:26 -0500

    Added navbar cart counting feature

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,21 @@
 import { Link } from 'react-router-dom';
 
-function Navbar(){
+
+function Navbar({cart}){
+
+    function getCartTotal(){
+        let num = 0;
+        cart.forEach((item) => {
+            num += item.qty;
+        })
+        return num;
+    }
+
     return (
         <div className='navBar'>
             <Link to="/">Home</Link>
             <Link to="shop">Shop</Link>
-            <Link to="cart">Cart</Link>
+            <Link to="cart">Cart {getCartTotal()? getCartTotal() : ''}</Link>
         </div>
     )
 }
